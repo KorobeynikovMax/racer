90a619e7802eea12c845ea952aab86baad3366fc
Max <korobeynikovmax@gmail.com>
2021-03-09 19:07:24 +0300

    collision with objects

--- road/RoadManager.java
+++ road/RoadManager.java
@@ -13,19 +13,22 @@ public class RoadManager {
     public static final int RIGHT_BORDER = RacerGame.WIDTH - LEFT_BORDER;
     private static final int FIRST_LANE_POSITION = 16;
     private static final int FOURTH_LANE_POSITION = 44;
+    private static final int PLAYER_CAR_DISTANCE = 12;
     private List<RoadObject> items = new ArrayList<>();
 
     private RoadObject createRoadObject(RoadObjectType type, int x, int y) {
         if (type == RoadObjectType.THORN) {
             return new Thorn(x, y);
-        } else return null;
+        } else {
+            return new Car(type, x, y);
+        }
     }
 
     private void addRoadObject(RoadObjectType type, Game game) {
         int x = game.getRandomNumber(FIRST_LANE_POSITION, FOURTH_LANE_POSITION);
         int y = -1 * RoadObject.getHeight(type);
         RoadObject roadObject = createRoadObject(type, x, y);
-        if (roadObject != null) {
+        if (roadObject != null && isRoadSpaceFree(roadObject)) {
             items.add(roadObject);
         }
     }
@@ -61,6 +64,7 @@ public class RoadManager {
 
     public void generateNewRoadObjects(Game game) {
         generateThorn(game);
+        generateRegularCar(game);
     }
 
     private void deletePassedItems() {
@@ -79,5 +83,21 @@ public class RoadManager {
             result = result || roadObject.isCollision(playerCar);
         }
         return result;
+    }
+
+    private void generateRegularCar(Game game) {
+        int carTypeNumber = game.getRandomNumber(4);
+        if (game.getRandomNumber(100) < 30) {
+            addRoadObject(RoadObjectType.values()[carTypeNumber], game);
+        }
+    }
+
+    private boolean isRoadSpaceFree(RoadObject object) {
+        for (RoadObject roadObject : items) {
+            if (roadObject.isCollisionWithDistance(object, PLAYER_CAR_DISTANCE)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
